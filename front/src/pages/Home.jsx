0407be3703502c589b57bf8ef932c715e60f1019
EdbergMartins <edbergmartins@gmail.com>
2023-10-09 16:46:24 -0300

    fit create front config

---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <h2>Home</h2>
+      <p>Bem-vindo à página inicial do aplicativo.</p>
+    </div>
+  );
+}
+
+export default Home;
